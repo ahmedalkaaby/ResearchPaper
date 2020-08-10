13c4c90d014b9257bed8062b4e33656a73a61347
K3BE <ahmed.13.alkaaby@gmail.com>
2020-08-10 04:03:37 +0300

    pls..

--- server.js
+++ server.js
@@ -1,65 +1,46 @@
 const express = require("express");
-const request = require("request");
-const bodyParser = require("body-parser");
-const token = "";
-// "EAADJcX8epGYBABYbUAvvIM1P92KSNQCRtxKXHKOdwp0OuZBSn9IniXsiyxdjFheSgwyHjoZAAW81KZCcoHhPfhKAZAUR6Psx2UPsZBKi6rr7WXF2h7FERLmb7xcgKyvJr9ladbsQqTNZBv5N8lwaZCu3ZBDdJ9qXKaL5shVYytPpC331G9eXWaqTh1tIG8NgYkkZD"
-
+const path = require("path");
+const axios = require("axios");
+// Loads env variables
+require("dotenv").config();
 const app = express();
-
-app.set("port", process.env.PORT || 5000);
-
-//allow us to process the data
-app.use(bodyParser.urlencoded({ textended: false }));
-app.use(bodyParser.json());
-
-//Routes
-app.get("/", function (req, res) {
-  res.send("Hi, Im Ahmed.. :)");
-});
-
-// Facebook
-app.get("/webhook/", function (req, res) {
-  if (req.query["hub.verify_token"] === "nabeelbot") {
-    res.send(req.query["hub.challenge"]);
-  }
-  res.send("ERROR! Wrong Token");
-});
-
-app.post("/webhook/", function (req, res) {
-  let messaging_events = req.body.entry[0].messaging;
-  for (let i = 0; i < messaging_events.length; i++) {
-    let event = messaging_events[i];
-    let sender = event.sender.id;
-    if (event.message && event.message.text) {
-      let text = event.message.text;
-      sendText(sender, "Text echo: " + text.substring(0, 100));
-    }
-  }
-  res.sendStatus(200);
-});
-
-function sendText(sender, text) {
-  let messageData = { text: text };
-  request(
-    {
-      url: "https://graph.facebook.com/v6.0/me/messages",
-      qs: { access_token: token },
-      method: "POST",
-      json: {
-        recipient: { id: sender },
-        message: messageData,
+const PORT = process.env.PORT || 3001;
+// Adds json parsing middleware
+app.use(express.json());
+// Setup static directory to serve
+app.use(express.static(path.resolve("client", "build")));
+// Creates weather endpoint
+app.post("/weather", async (req, res) => {
+  const { location } = req.body;
+  // Encode the variable so we can send the location in a URL
+  const encodedLocation = encodeURIComponent(location);
+  try {
+    // Call the Weather API
+    const { data } = await axios({
+      method: "GET",
+      url: `https://aerisweather1.p.rapidapi.com/observations/${encodedLocation}`,
+      headers: {
+        "content-type": "application/octet-stream",
+        "x-rapidapi-host": "aerisweather1.p.rapidapi.com",
+        "x-rapidapi-key": process.env.RAPIDAPI_KEY,
+        useQueryString: true,
       },
-    },
-    function (error, res, body) {
-      if (error) {
-        console.log("sending error");
-      } else if (res.body.error) {
-        console.log("response body error");
-      }
-    }
-  );
-}
-
-app.listen(app.get("port"), function () {
-  console.log("running: port");
+    });
+    // Pull the information that we need from the Weather API response
+    const weatherData = {
+      conditions: data.response.ob.weather,
+      tempC: data.response.ob.tempC,
+      tempF: data.response.ob.tempF,
+    };
+    // Return the data object
+    return res.send(weatherData);
+  } catch (e) {
+    console.log(e);
+    return res.status(500).send("Error.");
+  }
 });
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve("client", "build", "index.html"));
+});
+// console.log that your server is up and running
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
